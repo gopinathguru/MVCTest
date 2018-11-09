30138e1c34f9639caf1d7691d243b7c2e4569389
gopinathguru <42901723+gopinathguru@users.noreply.github.com>
2018-11-08 20:54:09 -0500

    Add project files.

---
+++ MVCTest/Controllers/TestController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTest.Models;
+
+namespace MVCTest.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            ViewBag.TestList = new List<string>()
+            {
+                "one","Two","Three","four"
+            };
+            return View();
+        }
+
+        // GET: Test/Details/5
+        public ActionResult Details()
+        {
+            int id = 1;
+            DBContext dBContext = new DBContext();
+            Employee employee = dBContext.Employee.Single(emp => emp.Id == id);
+
+            //Employee employee = new Employee()
+            //{
+            //    EmpId = 100,
+            //    FirstName = "first",
+            //    LastName = "Last"
+            //};
+
+
+            return View(employee);
+        }
+
+        // GET: Test/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Test/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Test/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Test/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCTest/Models/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCTest.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
